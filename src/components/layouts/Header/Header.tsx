d160d84f39ad516057d5e1080cc1c57df3799518
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-05-28 15:00:43 +0900

    first

---
+++ src/components/layouts/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/ModeToggle";
+
+export default function Header() {
+    return (
+        <header className="">
+            header
+            <ModeToggle />
+        </header>
+    )
+}
